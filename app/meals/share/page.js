7c71aab3d5ff1671fb819ac249752271d68e0eeb
sujith <sujith.phpdeveloper@gmail.com>
2025-07-03 17:15:09 +0400

    validation from server level

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,12 @@
+'use client';
 import classes from './page.module.css';
 import ImagePicker from "@/components/meals/image-picker";
 import {shareMeal} from '@/lib/actions';
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
+import {useFormState} from "react-dom";
 
 export default function ShareMealPage() {
+    const [state, formAction] = useFormState(shareMeal, {message: null});
     return (
         <>
             <header className={classes.header}>
@@ -13,7 +16,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form} action={shareMeal}>
+                <form className={classes.form} action={formAction}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -21,7 +24,7 @@ export default function ShareMealPage() {
                         </p>
                         <p>
                             <label htmlFor="email">Your email</label>
-                            <input type="email" id="email" name="email" required />
+                            <input type="email" id="email" name="email"  />
                         </p>
                     </div>
                     <p>
@@ -42,6 +45,7 @@ export default function ShareMealPage() {
                         ></textarea>
                     </p>
                     <ImagePicker label="Choose Image" name="image"/>
+                    {state.message && <p>{state.message}</p>}
                     <p className={classes.actions}>
                         <MealsFormSubmit />
                     </p>
