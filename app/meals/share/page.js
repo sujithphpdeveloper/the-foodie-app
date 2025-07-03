648f8e98f62379d60ebc1e752294c598db6b2e16
sujith <sujith.phpdeveloper@gmail.com>
2025-07-03 14:04:30 +0400

    Creating new meal, Form submission, Image picker, server side functions

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,7 @@
 import classes from './page.module.css';
 import ImagePicker from "@/components/meals/image-picker";
+import {shareMeal} from '@/lib/actions';
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
     return (
@@ -11,7 +13,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form}>
+                <form className={classes.form} action={shareMeal}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -41,7 +43,7 @@ export default function ShareMealPage() {
                     </p>
                     <ImagePicker label="Choose Image" name="image"/>
                     <p className={classes.actions}>
-                        <button type="submit">Share Meal</button>
+                        <MealsFormSubmit />
                     </p>
                 </form>
             </main>
